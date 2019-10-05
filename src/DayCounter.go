95fed170add3435982a77b9f1e3fadf23c34ad40
Niel Ponciano <nielgp93@gmail.com>
2019-10-05 17:41:56 +0800

    Added support for work leave days

--- src/DayCounter.go
+++ src/DayCounter.go
@@ -12,9 +12,15 @@ type Holiday struct {
     day int
 }
 
+type Leaveday struct {
+    month string
+    day int
+}
+
 func main() {
+  
   currentTime := time.Now()
-      currentTime = time.Date(2019, 9, 11, 0, 0, 0, 0, time.UTC) // hack due to incorrect time in this website
+      currentTime = time.Date(2019, 9, 12, 0, 0, 0, 0, time.UTC) // hack due to incorrect time in this website
   month := currentTime.Month()
   day := currentTime.Day()
   year := currentTime.Year()
@@ -31,12 +37,19 @@ func main() {
       Holiday{"November", 30},
       Holiday{"December", 8},
   }
+  leavedays := []Leaveday{
+      Leaveday{"November", 22},
+      Leaveday{"December", 17},
+      Leaveday{"December", 18},
+      Leaveday{"December", 19},
+      Leaveday{"December", 20},
+  }
 
   fmt.Println("Current date:", month, day, year)
   fmt.Println("End date:", endMonth, endDay, endYear)
   fmt.Println()
 
-  for daysCtr, workingDaysCtr := 1, 1; month.String() != endMonth || day != endDay || year != endYear; daysCtr++ {
+  for daysCtr, workingDaysCtr, weekCtr := 1, 1, 1; month.String() != endMonth || day != endDay || year != endYear; daysCtr++ {
     currentTime = currentTime.AddDate(0, 0, 1)
     month = currentTime.Month()
     day = currentTime.Day()
@@ -46,6 +59,8 @@ func main() {
     if weekDay == "Saturday" || weekDay == "Sunday" {
         fmt.Println("Day:", daysCtr, weekDay)
         continue
+    } else if weekDay == "Monday" {
+        weekCtr++
     }
 
     isHoliday := false
@@ -60,7 +75,20 @@ func main() {
         continue
     }
 
-    fmt.Println("Day:", daysCtr, weekDay, "WorkingDay:", workingDaysCtr , ":", month, day, year)
+
+    isLeaveday := false
+    for _, currentLeaveday := range leavedays {
+        if month.String() == currentLeaveday.month && day == currentLeaveday.day {
+            isLeaveday = true
+            break
+        }
+    }
+    if isLeaveday {
+        fmt.Println("Day:", daysCtr, "Leaveday")
+        continue
+    }
+
+    fmt.Println("Day:", daysCtr, weekDay, "WorkingDay:", workingDaysCtr , "Week:", weekCtr, ":", month, day, year)
     workingDaysCtr++
   }
 }
