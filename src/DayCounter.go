931202de715702b0a8077e10332a361b1fe9ead5
Niel Ponciano <nielgp93@gmail.com>
2019-10-05 17:56:38 +0800

    Refactored DayCounter into different modules

--- src/DayCounter.go
+++ src/DayCounter.go
@@ -7,88 +7,160 @@ import (
   "time"
 )
 
+// ----------------
+// DATE INFORMATION
+// ----------------
+
+type DateInfo struct {
+    timeData time.Time
+    year int
+    month time.Month
+    day int
+    weekDay string
+}
+
+func (dateInfo *DateInfo) addDate(years int, months int, days int) {
+    dateInfo.timeData = dateInfo.timeData.AddDate(years, months, 1)
+    dateInfo.year, dateInfo.month, dateInfo.day = dateInfo.timeData.Date()
+    dateInfo.weekDay = dateInfo.timeData.Format("Monday")
+}
+
+func (dateInfo DateInfo) isEarlierThan(other DateInfo) bool {
+    return dateInfo.year <= other.year &&
+            (dateInfo.month < other.month ||
+            (dateInfo.month == other.month && dateInfo.day < other.day))
+}
+
+func (dateInfo DateInfo) isWeekEnd() bool {
+    return dateInfo.weekDay == "Saturday" || dateInfo.weekDay == "Sunday"
+}
+
+func (dateInfo DateInfo) isWeekStart() bool {
+    return dateInfo.weekDay == "Monday"
+}
+
+func (dateInfo DateInfo) isHoliday(holidays []Holiday) bool {
+    for _, currentHoliday := range holidays {
+        if dateInfo.month.String() == currentHoliday.month && dateInfo.day == currentHoliday.day {
+            return true
+        }
+    }
+    return false
+}
+
+func (dateInfo DateInfo) isLeaveDay(leaveDays []LeaveDay) bool {
+    for _, currentLeaveDay := range leaveDays {
+        if dateInfo.month.String() == currentLeaveDay.month && dateInfo.day == currentLeaveDay.day {
+            return true
+        }
+    }
+    return false
+}
+
+func getStartDateInfo() DateInfo {
+    startTime := time.Now()
+
+    // <MODIFY THIS IF START TIME IS DIFFERENT>
+    startTime = time.Date(2019, 9, 14, 0, 0, 0, 0, time.UTC)
+    // </MODIFY THIS IF START TIME IS DIFFERENT>
+
+    year, month, day := startTime.Date()
+    weekDay := startTime.Format("Monday")
+    return DateInfo{startTime, year, month, day, weekDay}
+}
+
+func getTargetDateInfo() DateInfo {
+
+    // <MODIFY THIS IF TARGET TIME IS DIFFERENT>
+    targetTime := time.Date(2019, 12, 22, 0, 0, 0, 0, time.UTC)
+    // </MODIFY THIS IF TARGET TIME IS DIFFERENT>
+
+    year, month, day := targetTime.Date()
+    weekDay := targetTime.Format("Monday")
+    return DateInfo{targetTime, year, month, day, weekDay}
+}
+
+// -------------------
+// HOLIDAY INFORMATION
+// -------------------
+
 type Holiday struct {
     month string
     day int
 }
 
-type Leaveday struct {
+func getHolidays() []Holiday {
+
+    // <MODIFY THIS FOR LIST OF HOLIDAYS>
+    return []Holiday {
+        Holiday{"November", 1},
+        Holiday{"November", 2},
+        Holiday{"November", 30},
+        Holiday{"December", 8},
+    }
+    // </MODIFY THIS FOR LIST OF HOLIDAYS>
+
+}
+
+// -------------------
+// LEAVES INFORMATION
+// -------------------
+
+type LeaveDay struct {
     month string
     day int
 }
 
-func main() {
-  
-  currentTime := time.Now()
-      currentTime = time.Date(2019, 9, 12, 0, 0, 0, 0, time.UTC) // hack due to incorrect time in this website
-  month := currentTime.Month()
-  day := currentTime.Day()
-  year := currentTime.Year()
-  weekDay := currentTime.Format("Monday") // currentTime.Weekday()
+func getLeaveDays() []LeaveDay {
 
-  endMonth := "December"
-  endDay := 22
-  endYear := 2019
-  // endTime := time.Date(2019, 12, 22, 0, 0, 0, 0, time.UTC)
-  
-  holidays := []Holiday{
-      Holiday{"November", 1},
-      Holiday{"November", 2},
-      Holiday{"November", 30},
-      Holiday{"December", 8},
-  }
-  leavedays := []Leaveday{
-      Leaveday{"November", 22},
-      Leaveday{"December", 17},
-      Leaveday{"December", 18},
-      Leaveday{"December", 19},
-      Leaveday{"December", 20},
-  }
-
-  fmt.Println("Current date:", month, day, year)
-  fmt.Println("End date:", endMonth, endDay, endYear)
-  fmt.Println()
-
-  for daysCtr, workingDaysCtr, weekCtr := 1, 1, 1; month.String() != endMonth || day != endDay || year != endYear; daysCtr++ {
-    currentTime = currentTime.AddDate(0, 0, 1)
-    month = currentTime.Month()
-    day = currentTime.Day()
-    year = currentTime.Year()
-    weekDay = currentTime.Format("Monday")
-    
-    if weekDay == "Saturday" || weekDay == "Sunday" {
-        fmt.Println("Day:", daysCtr, weekDay)
-        continue
-    } else if weekDay == "Monday" {
-        weekCtr++
+    // <MODIFY THIS FOR LIST OF LEAVEDAYS>
+    return []LeaveDay {
+        LeaveDay{"November", 22},
+        LeaveDay{"December", 17},
+        LeaveDay{"December", 18},
+        LeaveDay{"December", 19},
+        LeaveDay{"December", 20},
     }
+    // </MODIFY THIS FOR LIST OF LEAVEDAYS>
 
-    isHoliday := false
-    for _, currentHoliday := range holidays {
-        if month.String() == currentHoliday.month && day == currentHoliday.day {
-            isHoliday = true
-            break
+}
+
+// -------------------
+// MAIN
+// -------------------
+
+func main() {  
+    dateInfo := getStartDateInfo()
+    targetDateInfo := getTargetDateInfo()
+
+    holidays := getHolidays()
+    leaveDays := getLeaveDays()
+
+    fmt.Println("StartDateInfo:", dateInfo)
+    fmt.Println("TargetDateInfo:", targetDateInfo)
+    fmt.Println()
+
+    for daysCtr, workingDaysCtr, weekCtr := 1, 1, 1; dateInfo.isEarlierThan(targetDateInfo); daysCtr++ {
+        dateInfo.addDate(0, 0, 1)
+
+        if dateInfo.isWeekEnd() {
+            fmt.Println("Day:", daysCtr, "WeekEnd")
+            continue
+        } else if dateInfo.isHoliday(holidays) {
+            fmt.Println("Day:", daysCtr, "Holiday")
+            continue
+        } else if dateInfo.isLeaveDay(leaveDays) {
+            fmt.Println("Day:", daysCtr, "LeaveDay")
+            continue
         }
-    }
-    if isHoliday {
-        fmt.Println("Day:", daysCtr, "Holiday")
-        continue
-    }
 
-
-    isLeaveday := false
-    for _, currentLeaveday := range leavedays {
-        if month.String() == currentLeaveday.month && day == currentLeaveday.day {
-            isLeaveday = true
-            break
+        if dateInfo.isWeekStart() {
+            weekCtr++
         }
+        fmt.Println("Day:", daysCtr, dateInfo.weekDay,
+                    "WorkingDay:", workingDaysCtr,
+                    "Week:", weekCtr,
+                    "Date:", dateInfo.month, dateInfo.day, dateInfo.year)
+        workingDaysCtr++
     }
-    if isLeaveday {
-        fmt.Println("Day:", daysCtr, "Leaveday")
-        continue
-    }
-
-    fmt.Println("Day:", daysCtr, weekDay, "WorkingDay:", workingDaysCtr , "Week:", weekCtr, ":", month, day, year)
-    workingDaysCtr++
-  }
 }
